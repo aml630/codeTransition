8c16f2ef7e1cc5e9a19e5ac3298c8c719ed914c1
Alex Larson and Erik Tolentino <alex.m.larson@gmail.com>
2016-01-26 09:46:12 -0800

    Add html, jquery, js functionality

--- spec/specs.js
+++ spec/specs.js
@@ -3,12 +3,19 @@ describe('Contact', function() {
   var testContact = new Contact ("Mike","Lor","235 SE.Street Portland, OR");
   expect(testContact.firstName).to.equal("Mike");
   expect(testContact.lastName).to.equal("Lor");
-  expect(testContact.address).to.eql(['235 SE.Street Portland, OR']);
+  expect(testContact.address).to.equal('235 SE.Street Portland, OR');
   });
 
   it("adds the fullName method to a contact", function(){
-  var testContact = new Contact ("Erik","Tolentino");
-  expect(testContact.fullName()).to.equal("Erik Tolentino");
+  var testContact = new Contact ("Erik","Tolentino", "3333");
+  expect(testContact.fullName()).to.equal("Erik Tolentino 3333");
   });
-
 });
+
+
+describe('Address', function () {
+  it('will combine all the address fields', function () {
+    var testAddress = new Address("happyLane", "Portland", "OR");
+    expect(testAddress.fullAddress()).to.equal("happyLane Portland OR")
+  })
+})
