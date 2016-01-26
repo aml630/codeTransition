3196c71c671d8e7d95d6a8c9e8aeaf5062e05a56
Alex Larson and Erik Tolentino <tolentino3575@gmail.com>
2016-01-26 10:57:37 -0800

    scripts passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
 
+function Ticket(time, age, matinee) {
+  this.time = time;
+  this.age = age;
+  this.matinee = matinee;
+};
+
+
+Ticket.prototype.newMovie = function() {
+  if (this.time >= 10) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Ticket.prototype.senior = function(){
+  if (this.age >= 55) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Ticket.prototype.isMatinee = function() {
+  if (this.matinee < 12) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
 
 $(document).ready(function() {
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,27 @@
-describe('Contact', function() {
-  it("creates a new contact with the given properties.", function(){
-  var testContact = new Contact ("Mike","Lor","235 SE.Street Portland, OR");
-  expect(testContact.firstName).to.equal("Mike");
-  expect(testContact.lastName).to.equal("Lor");
-  expect(testContact.address).to.equal('235 SE.Street Portland, OR');
+
+describe('Ticket', function() {
+  it("Display Ticket Object Inputs", function () {
+    var testTicket = new Ticket(20, 55, 9);
+    expect(testTicket.time).to.equal(20);
+    expect(testTicket.age).to.equal(55);
+    expect(testTicket.matinee).to.equal(9);
   });
+
+
+  it("should charge more for new movies", function(){
+    var testTicket = new Ticket(20, 55, 9);    expect(testTicket.newMovie()).to.equal(true);
+  });
+
+  it("should charge less for old movies", function(){
+    var testTicket = new Ticket(2, 55, 9);    expect(testTicket.newMovie()).to.equal(false);
+  });
+
+  it("should discount for old people", function(){
+    var testTicket = new Ticket(20, 55, 9);    expect(testTicket.senior()).to.equal(true);
+
+  });
+
+  it("should discount for matinee times", function() {
+    var testTicket = new Ticket(20, 55, 9);    expect(testTicket.isMatinee()).to.equal(true);
+  });
+});
